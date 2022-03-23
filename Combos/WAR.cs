a344a4075fe32ca62502dfdd04b091b367d2a787
Lilith Song <lsong@princessrtfm.com>
2022-03-23 09:25:08 -0400

    Correct checks for WAR combos
    The Warrior gauge overcap savers should no longer interrupt combos midway through; they now check that you are NOT in the middle of one before applying. Closes #30.

--- Combos/WAR.cs
+++ Combos/WAR.cs
@@ -44,6 +44,7 @@ namespace XIVComboVX.Combos {
 			public const byte
 				Maim = 4,
 				Berserk = 6,
+				Overpower = 10,
 				StormsPath = 26,
 				ThrillOfBattle = 30,
 				InnerBeast = 35,
@@ -83,13 +84,16 @@ namespace XIVComboVX.Combos {
 				);
 			}
 
-			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapPathFeature) && level >= WAR.Levels.InnerBeast && GetJobGauge<WARGauge>().BeastGauge > 70)
-				return OriginalHook(WAR.FellCleave);
-
-			return SimpleChainCombo(level, lastComboMove, comboTime, (1, WAR.HeavySwing),
+			uint nextMainCombo = 0;
+			bool inMainCombo = comboTime > 0 && PartialChainCombo(level, lastComboMove, out nextMainCombo, (1, WAR.HeavySwing),
 				(WAR.Levels.Maim, WAR.Maim),
 				(WAR.Levels.StormsPath, WAR.StormsPath)
 			);
+
+			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapPathFeature) && level >= WAR.Levels.InnerBeast && !inMainCombo && GetJobGauge<WARGauge>().BeastGauge > 70)
+				return OriginalHook(WAR.FellCleave);
+
+			return inMainCombo ? nextMainCombo : WAR.HeavySwing;
 		}
 	}
 
@@ -102,13 +106,16 @@ namespace XIVComboVX.Combos {
 			if (IsEnabled(CustomComboPreset.WarriorInnerReleaseFeature) && level >= WAR.Levels.InnerRelease && SelfHasEffect(WAR.Buffs.InnerRelease))
 				return OriginalHook(WAR.FellCleave);
 
-			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapEyeFeature) && level >= WAR.Levels.InnerRelease && GetJobGauge<WARGauge>().BeastGauge > 80)
-				return OriginalHook(WAR.FellCleave);
-
-			return SimpleChainCombo(level, lastComboMove, comboTime, (1, WAR.HeavySwing),
+			uint nextMainCombo = 0;
+			bool inMainCombo = comboTime > 0 && PartialChainCombo(level, lastComboMove, out nextMainCombo, (1, WAR.HeavySwing),
 				(WAR.Levels.Maim, WAR.Maim),
 				(WAR.Levels.StormsEye, WAR.StormsEye)
 			);
+
+			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapEyeFeature) && level >= WAR.Levels.InnerBeast && !inMainCombo && GetJobGauge<WARGauge>().BeastGauge > 80)
+				return OriginalHook(WAR.FellCleave);
+
+			return inMainCombo ? nextMainCombo : WAR.HeavySwing;
 		}
 	}
 
@@ -122,13 +129,15 @@ namespace XIVComboVX.Combos {
 			if (IsEnabled(CustomComboPreset.WarriorInnerReleaseFeature) && SelfHasEffect(WAR.Buffs.InnerRelease))
 				return OriginalHook(WAR.Decimate);
 
-			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapTempestFeature) && level >= WAR.Levels.MythrilTempestTrait && GetJobGauge<WARGauge>().BeastGauge > 80)
+			uint nextMainCombo = 0;
+			bool inMainCombo = comboTime > 0 && PartialChainCombo(level, lastComboMove, out nextMainCombo, (WAR.Levels.Overpower, WAR.Overpower),
+				(WAR.Levels.MythrilTempest, WAR.MythrilTempest)
+			);
+
+			if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapTempestFeature) && level >= WAR.Levels.MythrilTempestTrait && !inMainCombo && GetJobGauge<WARGauge>().BeastGauge > 80)
 				return OriginalHook(WAR.Decimate);
 
-			if (comboTime > 0 && lastComboMove == WAR.Overpower && level >= WAR.Levels.MythrilTempest)
-				return WAR.MythrilTempest;
-
-			return WAR.Overpower;
+			return inMainCombo ? nextMainCombo : WAR.Overpower;
 		}
 	}
 
