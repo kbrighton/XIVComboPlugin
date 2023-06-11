3cb659ab3cf23a98bc0817d5d48c858b8a88efd7
Lilith Song <lsong@princessrtfm.com>
2023-06-10 20:52:46 -0400

    Move suppressions into assembly-wide file

---
+++ XIVComboVX/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Standard formatting layout for combo classes")]
