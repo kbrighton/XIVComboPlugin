4c24f48af4ecb80d17805a2c6037faa7e06a8085
Lilith Song <lsong@princessrtfm.com>
2023-09-03 04:33:20 -0400

    remove unnecessary suppressions (moved to editorconfig)

--- XIVComboVX/LogUtil.cs
+++ XIVComboVX/LogUtil.cs
@@ -32,17 +32,14 @@ internal class LogUtil: IDisposable {
 		this.EnabledNextTick = false;
 	}
 
-	[SuppressMessage("Usage", "CA2201:Do not raise reserved exception types", Justification = "Exception is used ONLY to provide a stack trace, never thrown")]
 	internal void fatal(string msg, Exception? cause = null) {
 		if (this.Enabled)
 			PluginLog.Fatal($"{msg}\n{cause ?? new Exception(STACK_TRACE_MSG)}");
 	}
-	[SuppressMessage("Usage", "CA2201:Do not raise reserved exception types", Justification = "Exception is used ONLY to provide a stack trace, never thrown")]
 	internal void error(string msg, Exception? cause = null) {
 		if (this.Enabled)
 			PluginLog.Error($"{msg}\n{cause ?? new Exception(STACK_TRACE_MSG)}");
 	}
-	[SuppressMessage("Usage", "CA2201:Do not raise reserved exception types", Justification = "Exception is used ONLY to provide a stack trace, never thrown")]
 	internal void warning(string msg, Exception? cause = null) {
 		if (this.Enabled)
 			PluginLog.Warning($"{msg}\n{cause ?? new Exception(STACK_TRACE_MSG)}");
