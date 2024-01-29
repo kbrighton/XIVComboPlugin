6de24d1a3c9275db56972232d2fd19305012d466
Lilith Song <lsong@princessrtfm.com>
2024-01-28 21:32:35 -0500

    suppress a dumbshit code style rule

--- XIVComboVX/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Standard formatting layout for combo classes")]
---
+++ XIVComboVX/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Standard formatting layout for combo classes")]
+[assembly: SuppressMessage("Usage", "CA2201:Do not raise reserved exception types", Justification = "it's a stupid rule")]
