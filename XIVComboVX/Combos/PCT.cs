432ed61623a4139431198b5534fcf6ab6013c8e7
Frigidwalrus <ljcomplex@gmail.com>
2024-07-05 21:47:07 -0500

    Added PCT and Framed oout skills, buffs, and debuffs

---
+++ XIVComboVX/Combos/PCT.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dalamud.Game.ClientState.JobGauge.Types;
+
+namespace PrincessRTFM.XIVComboVX.Combos;
+internal class PCT {
+	public const byte JobID = 42;
+
+	public const uint
+		FireInRed = ushort.MaxValue,
+		TemperaCoat = ushort.MaxValue,
+		Smudge = ushort.MaxValue,
+		Fire2InRed = ushort.MaxValue,
+		CreatureMotif = ushort.MaxValue,
+		LivingMuse = ushort.MaxValue,
+		MogOfTheAges = ushort.MaxValue,
+		WeaponMotif = ushort.MaxValue,
+		SteelMuse = ushort.MaxValue,
+		HammerStamp = ushort.MaxValue,
+		BlizzardInCyan = ushort.MaxValue,
+		Blizzard2InCyan = ushort.MaxValue,
+		SubtractivePalette = ushort.MaxValue,
+		LandscapeMotif = ushort.MaxValue,
+		ScenicMuse = ushort.MaxValue,
+		HolyInWhite = ushort.MaxValue,
+		TemperaGrassa = ushort.MaxValue,
+		CometInBlack = ushort.MaxValue,
+		RaindbowDrip = ushort.MaxValue,
+		StarPrism = ushort.MaxValue,
+		// The following cannot be added to the hotbar
+		AeroInGreen = ushort.MaxValue,
+		WaterInBlue = ushort.MaxValue,
+		PomMotif = ushort.MaxValue,
+		WingMotif = ushort.MaxValue,
+		PomMuse = ushort.MaxValue,
+		WingedMuse = ushort.MaxValue,
+		Aero2InGreen = ushort.MaxValue,
+		Water2InGreen = ushort.MaxValue,
+		HammerMotif = ushort.MaxValue,
+		StrikingMuse = ushort.MaxValue,
+		StoneInYellow = ushort.MaxValue,
+		ThunderInMagenta = ushort.MaxValue,
+		Stone2InYellow = ushort.MaxValue,
+		Thunder2InMagenta = ushort.MaxValue,
+		StarrySkyMotif = ushort.MaxValue,
+		StarryMuse = ushort.MaxValue,
+		HammerBrush = ushort.MaxValue,
+		PolishingHammer = ushort.MaxValue,
+		ClawMotif = ushort.MaxValue,
+		MawMotif = ushort.MaxValue,
+		ClawedMuse = ushort.MaxValue,
+		FangedMuse = ushort.MaxValue,
+		RetributionOfTheMadeen = ushort.MaxValue;
+
+	public static class Buffs {
+		public const ushort
+			BuffPH = ushort.MaxValue;
+	}
+
+	public static class Debuffs {
+		public const ushort
+			DebuffPH = ushort.MaxValue;
+	}
+
+	public static class Levels {
+		public const byte
+			 FireInred = 1;
+
+	}
+		
+}
