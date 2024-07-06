47cf8e9da02b3fce6ac0433d97d66c839a61e0e4
Lilith Song <lsong@princessrtfm.com>
2024-07-05 22:30:54 -0400

    add VPR's headers

---
+++ XIVComboVX/Combos/VPR.cs
@@ -0,0 +1,100 @@
+namespace PrincessRTFM.XIVComboVX.Combos;
+
+internal static class VPR {
+	public const byte JobID = 255; // XXX
+
+	public const uint
+		SteelFangs = ushort.MaxValue,
+		HuntersSting = ushort.MaxValue,
+		DreadFangs = ushort.MaxValue,
+		WrithingSnap = ushort.MaxValue,
+		SwiftskinsSting = ushort.MaxValue,
+		SteelMaw = ushort.MaxValue,
+		FlankstingStrike = ushort.MaxValue,
+		FlanksbaneFang = ushort.MaxValue,
+		HindstingStrike = ushort.MaxValue,
+		HindsbaneFang = ushort.MaxValue,
+		DreadMaw = ushort.MaxValue,
+		Slither = ushort.MaxValue,
+		HuntersBite = ushort.MaxValue,
+		SwiftskinsBite = ushort.MaxValue,
+		JaggedMaw = ushort.MaxValue,
+		BloodiedMaw = ushort.MaxValue,
+		SerpentsTail = ushort.MaxValue,
+		DeathRattle = ushort.MaxValue,
+		LastLash = ushort.MaxValue,
+		Dreadwinder = ushort.MaxValue,
+		HuntersCoil = ushort.MaxValue,
+		SwiftskinsCoil = ushort.MaxValue,
+		PitOfDread = ushort.MaxValue,
+		HuntersDen = ushort.MaxValue,
+		SwiftskinsDen = ushort.MaxValue,
+		Twinfang = ushort.MaxValue,
+		Twinblood = ushort.MaxValue,
+		TwinfangBite = ushort.MaxValue,
+		TwinbloodBite = ushort.MaxValue,
+		TwinfangThresh = ushort.MaxValue,
+		TwinbloodThresh = ushort.MaxValue,
+		UncoiledFury = ushort.MaxValue,
+		SerpentsIre = ushort.MaxValue,
+		Reawaken = ushort.MaxValue,
+		FirstGeneration = ushort.MaxValue,
+		SecondGeneration = ushort.MaxValue,
+		ThirdGeneration = ushort.MaxValue,
+		FourthGeneration = ushort.MaxValue,
+		UncoiledTwinfang = ushort.MaxValue,
+		UncoiledTwinblood = ushort.MaxValue,
+		Ouroboros = ushort.MaxValue,
+		FirstLegacy = ushort.MaxValue,
+		SecondLegacy = ushort.MaxValue,
+		ThirdLegacy = ushort.MaxValue,
+		FourthLegacy = ushort.MaxValue;
+
+	public static class Buffs {
+		public const ushort
+			HuntersInstinct = ushort.MaxValue,
+			Swiftscaled = ushort.MaxValue,
+			FlankstungVenom = ushort.MaxValue,
+			HindstungVenom = ushort.MaxValue,
+			FlanksbaneVenom = ushort.MaxValue,
+			HindsbaneVenom = ushort.MaxValue;
+	}
+
+	public static class Debuffs {
+		public const ushort
+			NoxiousGnash = ushort.MaxValue;
+	}
+
+	public static class Levels {
+		public const byte
+			SteelFangs = 1,
+			HuntersSting = 5,
+			DreadFangs = 10,
+			WrithingSnap = 15,
+			SwiftskinsSting = 20,
+			SteelMaw = 25,
+			FlankstingStrike = 30,
+			DreadMaw = 35,
+			Slither = 40,
+			HuntersBite = 40,
+			SwiftskinsBite = 45,
+			JaggedMaw = 50,
+			DeathRattle = 55,
+			LastLash = 60,
+			Dreadwinder = 65,
+			HuntersCoil = 65,
+			SwiftskinsCoil = 65,
+			PitOfDread = 70,
+			HuntersDen = 70,
+			SwiftskinsDen = 70,
+			TwinfangBite = 75,
+			TwinfangThresh = 80,
+			UncoiledFury = 82,
+			SerpentsIre = 86,
+			Reawaken = 90,
+			Generations = 90,
+			UncoiledTwinfang = 92,
+			Ouroboros = 96,
+			Legacies = 100;
+	}
+}
